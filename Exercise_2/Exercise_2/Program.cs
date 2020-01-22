7788f01dcc5da893dfa461925e4c2fb4e84df5f2
deadartt <deadartt@gmail.com>
2020-01-22 17:10:30 +0400

    Second Exercise

---
+++ Exercise_2/Exercise_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose List size");
+            int listSize = Convert.ToInt32(Console.ReadLine());
+
+            List<int> collection = new List<int>();
+
+            for (int i = 0; i < listSize; i++)
+            {
+                Console.WriteLine("Add next element to list");
+                collection.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine($"Your elements in old list:");
+
+            foreach (var item in collection)
+            {
+                Console.WriteLine( item);
+            }
+
+            var newCollection = RemoveNonPrime.Remove(collection);
+
+            Console.WriteLine($"Your elements in new list without Prime");
+            foreach (var item in newCollection) 
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+        }
+    }
+}
---
+++ Exercise_2/Exercise_2/RemoveNonPrime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    class RemoveNonPrime
+    {
+        public static List<int> Remove(List<int> collection)
+        {
+            List<int> Result = new List<int>();
+
+            bool isPrime = true;
+
+            Parallel.ForEach(collection, element =>
+             {
+                 Parallel.For(2, element / 2 + 1, (int n, ParallelLoopState state) =>
+                    {
+                        if (element % 2 == 0)
+                        {
+                            isPrime = false;
+                            state.Break();
+                        }
+                    });
+
+                 if (isPrime)
+                 {
+                     Result.Add(element);
+                 }
+                 isPrime = true;
+             });
+
+            return Result;
+        }
+    }
+}
